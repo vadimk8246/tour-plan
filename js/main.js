8c8fadd40a5701c5469f2128706ded66508a027d
vadimk8246 <poltavec.vadim@mail.ru>
2020-12-11 14:00:04 +0300

    fixes: more fixes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,86 @@
-$(document).ready(function(){new Swiper(".hotel-slider",{loop:!0,navigation:{nextEl:".hotel-slider__button--next",prevEl:".hotel-slider__button--prev"},keyboard:{enabled:!0},effect:"fade"}),new Swiper(".reviews-slider",{loop:!0,navigation:{nextEl:".reviews-slider__button--next",prevEl:".reviews-slider__button--prev"}});document.querySelector(".menu-button").addEventListener("click",function(){document.querySelector(".nav-wrapper").classList.toggle("nav-wrapper__visible")});var e=$("[data-toggle=modal]"),o=$(".modal__close");function a(e){e.preventDefault();var o=$(".modal__overlay"),a=$(".modal__dialog");o.removeClass("modal__overlay--visible"),a.removeClass("modal__dialog--visible")}e.on("click",function(){var e=$(".modal__overlay"),o=$(".modal__dialog");e.addClass("modal__overlay--visible"),o.addClass("modal__dialog--visible")}),o.on("click",a),document.onkeydown=function(e){"Escape"==e.key&&a(event)},$(".form").each(function(){$(this).validate({errorClass:"invalid",messages:{name:{required:"Please specify your name"},modemail:{required:"We need your email address to contact you",modemail:"Your email address must be in the format of mail@domain.com"},search:{required:""}}})}),$.validator.methods.email=function(e,o){return this.optional(o)||/[a-z]+@[a-z]+\.[a-z]+/.test(e)},$(".input__phone").mask("+7 (000) 000-0000"),AOS.init()});
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  keyboard: {
+  enabled: true,
+  },
+  effect:"fade",
+})
+
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+   },
+  })
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener('click', function (){
+    document.querySelector(".nav-wrapper").classList.toggle('nav-wrapper__visible')
+  });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  // Открытие модального окна при нажатии на кнопку
+  function openModal(){
+    var modalOverlay = $ (".modal__overlay");
+    var modalDialog = $ (".modal__dialog");
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  };
+
+  // Закрытие модального окна с помощью крестика
+  function closeModal(event){
+    event.preventDefault();
+    var modalOverlay = $ (".modal__overlay");
+    var modalDialog = $ (".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  };
+
+  //Закрытие модального окна нажатием Escape
+  function closeEscape(e){
+    if (e.key == 'Escape'){
+        closeModal(event)
+    }
+  }
+  document.onkeydown = closeEscape
+
+
+  // Обработка форма
+  $(".form").each(function () {
+    $(this).validate({
+    errorClass:"invalid",
+    messages: {
+      name: {
+        required:"Please specify your name",
+      },
+      modemail: {
+        required: "We need your email address to contact you",
+        modemail: "Your email address must be in the format of mail@domain.com",
+      },
+      search:{
+        required:""
+      },
+      
+    },
+  });
+  })
+  $.validator.methods.email = function( value, element ) {
+  return this.optional( element ) || /[a-z,1-9]+@[a-z]+\.[a-z]+/.test( value );
+}
+  $('.input__phone').mask('+7 (000) 000-0000')
+  AOS.init();
+});
